2e9d2527af2aac08d19afacc503763e09b5a8ba9
RYE <robbyeadam@gmail.com>
2024-07-06 17:22:06 +0700

    added delete endpoint

---
+++ src/Services/Athlete/Athlete.API/Players/DeletePlayer/DeletePlayerEndpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace Athlete.API.Players.DeletePlayer
+{
+    public record DeletePlayerResponse(bool IsSuccess);
+
+
+    public class DeletePlayerEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/api/players/{id}", async (int id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeletePlayerCommand(id));
+                var response = result.Adapt<DeletePlayerResponse>();
+                return Results.Ok(response);
+            })
+                .WithName("DeletePlayer")
+                .Produces<DeletePlayerResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .ProducesProblem(StatusCodes.Status404NotFound);
+        }
+    }
+}
---
+++ src/Services/Athlete/Athlete.API/Players/DeletePlayer/DeletePlayerHandler.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Athlete.API.Players.DeletePlayer
+{
+    public record DeletePlayerCommand(
+    int Id) : ICommand<DeletePlayerResult>;
+
+    public record DeletePlayerResult(bool IsSuccess);
+
+    public class DeletePlayerCommandHandler(
+        PlayerContext context) : ICommandHandler<DeletePlayerCommand, DeletePlayerResult>
+    {
+        public async Task<DeletePlayerResult> Handle(DeletePlayerCommand command, CancellationToken cancellationToken)
+        {
+            var player = await context.Players.FindAsync(command.Id);
+            if (player != null)
+            {
+                context.Players.Remove(player);
+            }
+
+            await context.SaveChangesAsync(cancellationToken);
+            return new DeletePlayerResult(true);
+        }
+    }
+}
